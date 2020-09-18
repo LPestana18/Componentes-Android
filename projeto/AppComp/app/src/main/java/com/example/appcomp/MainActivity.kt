37b372f413ba974d45b833fe27ae50939d540d48
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-18 15:03:48 -0300

    spinner_dynamic

--- projeto/AppComp/app/src/main/java/com/example/appcomp/MainActivity.kt
+++ projeto/AppComp/app/src/main/java/com/example/appcomp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         button_toast.setOnClickListener(this)
         button_snack.setOnClickListener(this)
+
+        loadSpinner()
     }
 
     override fun onClick(v: View) {
@@ -47,6 +50,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 snack.show()
             }
         }
+    }
+
+    private fun loadSpinner() {
+        val mList = listOf("Gramas", "Kg", "Arroba", "Tonelada")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, mList)
+        spinner_dynamic.adapter = adapter
     }
 
     private fun toast(str: String) {
