94d06abd1b3591003acd71ed1af852396c039662
Lucas Ribeiro Pestana <68862058+LPestana18@users.noreply.github.com>
2020-09-18 16:10:11 -0300

    events spinner

--- projeto/AppComp/app/src/main/java/com/example/appcomp/MainActivity.kt
+++ projeto/AppComp/app/src/main/java/com/example/appcomp/MainActivity.kt
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener, AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button_toast.setOnClickListener(this)
         button_snack.setOnClickListener(this)
+        button_get_spinner.setOnClickListener(this)
+        button_set_spinner.setOnClickListener(this)
+
+        spinner_static.onItemSelectedListener = this
 
         loadSpinner()
     }
@@ -49,6 +54,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
                 snack.show()
             }
+            R.id.button_get_spinner -> {
+                val selectedItem = spinner_static.selectedItem
+                val selectedItemId = spinner_static.selectedItemId
+                val selectedItemPosition = spinner_static.selectedItemPosition
+
+                toast("Position: $selectedItemId: $selectedItem")
+            }
+            R.id.button_set_spinner -> {
+                spinner_static.setSelection(2)
+            }
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        toast("nothing")
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when (parent?.id) {
+            R.id.spinner_static -> {
+                toast(parent?.getItemAtPosition(position).toString())
+            }
         }
     }
 
@@ -59,7 +86,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun toast(str: String) {
-        val toast = Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+        val toast = Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
 
     }
 
